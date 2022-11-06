17c54317f150802064447f9a4259cc0fe5b78bc8
DizzyShadoWWW <73545057+DizzyShadoWWW@users.noreply.github.com>
2022-11-07 01:07:27 +0530

    made navbar responsive

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,20 +1,22 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {Link,NavLink, useLocation} from 'react-router-dom'
 import './Styles/Navbar.css'
+import { FiShoppingCart } from "react-icons/fi";
+import {CgMenu, CgClose} from 'react-icons/cg'
 
 
 
 function Navbar() {
-
+    const [menuIcon, setMenuIcon] = useState();
     let location = useLocation();
   return (
     <div>
-        <nav className="navbar navbar-expand-lg ">
+        <nav className={menuIcon ? "navbar navbar-expand-lg navbar-dark isactive":"navbar navbar-expand-lg navbar-dark"}>
             <div className="container-fluid ">
-                <Link className="navbar-brand" to="/">Orion</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+                <Link className="logo" to="/">
+                    <img src="https://i.ibb.co/R9xRWQd/Orion.png" alt="Orion"/>
+                </Link>
+                      
                 <div className="d-flex flex-end" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
@@ -29,10 +31,26 @@ function Navbar() {
                         <li className="nav-item">
                             <NavLink className={`nav-link ${location.pathname==="/products"? "active-link": ""}`} to="/products">Products</NavLink>
                         </li>
-                        <li>
-                        <NavLink className={"nav-link"}>Cart</NavLink>
+                        </ul>                    
+                </div>
+                <div className='d-flex flex-end trolley'>
+                        <ul className="me-auto mb-2 mb-lg-0">                
+                        <li className="nav-item cart">
+                        <NavLink className={`nav-link cart-trolley--link ${location.pathname==="/cart"? "active-link": ""}`} to="/cart">
+                            <FiShoppingCart className="cart-trolley"/>
+                            <span className="cart-total--item">10</span>
+                        </NavLink>
                         </li>
-                    </ul>
+                     </ul>
+                </div>
+                {/*  Hamburger button for Mobile    */} 
+                <div className="mobile-navbar-btn">
+                    <CgMenu name="menu-outline" className="mobile-nav-icon"
+                    onClick={() => setMenuIcon(true)}
+                    />
+                    <CgClose name="close-outline" className="mobile-nav-icon close-outline"
+                    onClick={() => setMenuIcon(false)}
+                    />
                 </div>               
             </div>
         </nav>
