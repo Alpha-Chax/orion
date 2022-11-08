17a8bff28a8b0d5f86304889fdb0a98c3f0bd007
DizzyShadoWWW <73545057+DizzyShadoWWW@users.noreply.github.com>
2022-11-08 18:44:28 +0530

    updated banner

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -5,17 +5,23 @@ import './Styles/Banner.css'
 const Banner = () => {
   return (
     <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
-  <div className="carousel-inner">
-    <div className="carousel-item active" data-bs-interval="4000">
+    <div className="carousel-inner">
+      <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+    </div>
+    <div className="carousel-item active" data-bs-interval="2000">
       <img src="https://uptownstudios.net/wp-content/uploads/2019/11/Uptown-Blog-2019-AccessibleColor-1.jpg" className="d-block w-100" alt="..."/>
     </div>
-    <div className="carousel-item">
+    <div className="carousel-item" data-bs-interval="2000">
       <img src="https://i.ytimg.com/vi/ETsekJKsr3M/maxresdefault.jpg" className="d-block w-100" alt="..."/>
     </div>
-    <div className="carousel-item">
+    <div className="carousel-item" data-bs-interval="2000">
       <img src="https://mobirise.com/extensions/commercem4/assets/images/gallery06.jpg" className="d-block w-100" alt="..."/>
     </div>
-    <div className="carousel-item">
+    <div className="carousel-item" data-bs-interval="2000">
       <img src="https://images.unsplash.com/photo-1622324178857-0d889ffee00b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGJsYWNrJTIwZ3VpdGFyfGVufDB8fDB8fA%3D%3D&w=1000&q=80" className="d-block w-100" alt="..."/>
     </div>
   </div>
