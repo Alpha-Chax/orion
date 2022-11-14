7c7bac62ecbc35b3ca29a2e1e9f52ff55099dd60
DizzyShadoWWW <73545057+DizzyShadoWWW@users.noreply.github.com>
2022-11-14 14:44:49 +0530

    formatting

--- src/App.js
+++ src/App.js
@@ -1,40 +1,34 @@
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
-import Home from './Components/Pages/Home'
-import About from './Components/Pages/About'
-import Navbar from './Components/Navbar';
-import Products from './Components/Pages/Products';
-import Contact from './Components/Pages/Contact';
-import Cart from './Components/Cart';
-import Footer from './Components/Footer';
-import ErrorPage from './Components/ErrorPage';
-import SingleProduct from './Components/SingleProduct';
-import ScrollToTop from './Components/ScrollToTop';
-
-
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Components/Pages/Home";
+import About from "./Components/Pages/About";
+import Navbar from "./Components/Navbar";
+import Products from "./Components/Pages/Products";
+import Contact from "./Components/Pages/Contact";
+import Cart from "./Components/Cart";
+import Footer from "./Components/Footer";
+import ErrorPage from "./Components/ErrorPage";
+import SingleProduct from "./Components/SingleProduct";
+import ScrollToTop from "./Components/ScrollToTop";
 
 function App() {
   return (
     <>
-    <Router>
-    <Navbar/>
-    <ScrollToTop/>
-    <Routes>
-    <Route exact path="/" element={<Home/>}/> 
-    <Route exact path="/about" element={<About/>}/>
-    <Route exact path="/products" element={<Products/>}/>
-    <Route exact path="/products/:id" element={<SingleProduct/>}/>
-    <Route exact path="/contact" element={<Contact/>}/>
-    <Route exact path="/cart" element={<Cart/>}/>
-    <Route path="*" element={<ErrorPage/>}/>
-    </Routes>
-    <Footer/>
-    </Router>
-   </>
+      <Router>
+        <Navbar />
+        <ScrollToTop />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/products" element={<Products />} />
+          <Route exact path="/products/:id" element={<SingleProduct />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/cart" element={<Cart />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
--- src/Components/SingleProduct.js
+++ src/Components/SingleProduct.js
@@ -1,27 +1,24 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch} from 'react-redux';
-import {addCart} from '../Redux/action/index.js';
+import { useDispatch } from "react-redux";
+import { addCart } from "../Redux/action/index.js";
 import { useParams } from "react-router";
 import ShowProduct from "./Pages/ShowProduct";
 import SpLoader from "./SpLoader";
 import { NavLink } from "react-router-dom";
-
-
-
 
 const SingleProduct = () => {
   const { id } = useParams();
   const [product, setProduct] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const handleAddProduct = ()=> {
+  const handleAddProduct = () => {
     return addProduct(product);
-   }
-  
+  };
+
   const dispatch = useDispatch();
-    const addProduct = (result) => {
-      dispatch(addCart(result));
-    }
+  const addProduct = (result) => {
+    dispatch(addCart(result));
+  };
 
   const getProduct = async () => {
     setLoading(true);
@@ -35,35 +32,45 @@ const SingleProduct = () => {
   useEffect(() => {
     setTimeout(() => {
       getProduct();
-    },1500 );  
+    }, 1500);
     //eslint-disable-next-line
   }, []);
 
   return (
     <>
-    <div className="container py-5">
-      <div className="row py-4 col-md-14">
-        {loading && <SpLoader/>}
-        {product.map((element) => {
-          return (
-            <>
-            <ShowProduct 
-              title={element.title ? element.title : ""} 
-              description={element.description ? element.description : ""}
-              images={element.images[1]}
-              price={element.price}
-              rating={element.rating}
-              category={element.category}
-            />
-            <div className="my-3  d-flex justify-content-center">
-            <NavLink className="btn btn-outline-dark mx-4 px-3 py-2" to="/cart">Go To Cart</NavLink>
-            <button className="btn btn-outline-dark px-3 py-2" onClick={handleAddProduct}>Add To Cart</button>
-            </div> 
-            </>
-          );
-        })}    
+      <div className="container py-5">
+        <div className="row py-4 col-md-14">
+          {loading && <SpLoader />}
+          {product.map((element) => {
+            return (
+              <>
+                <ShowProduct
+                  title={element.title ? element.title : ""}
+                  description={element.description ? element.description : ""}
+                  images={element.images[1]}
+                  price={element.price}
+                  rating={element.rating}
+                  category={element.category}
+                />
+                <div className="my-3  d-flex justify-content-center">
+                  <NavLink
+                    className="btn btn-outline-dark mx-4 px-3 py-2"
+                    to="/cart"
+                  >
+                    Go To Cart
+                  </NavLink>
+                  <button
+                    className="btn btn-outline-dark px-3 py-2"
+                    onClick={handleAddProduct}
+                  >
+                    Add To Cart
+                  </button>
+                </div>
+              </>
+            );
+          })}
+        </div>
       </div>
-    </div>
     </>
   );
 };
